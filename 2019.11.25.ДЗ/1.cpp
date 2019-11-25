6411cb8cc660e9434110723d3bf506e9f38374cf
GerVan <57504043+GerVan@users.noreply.github.com>
2019-11-25 19:31:32 +0300

    Update 1.cpp

--- 2019.11.25.ДЗ/1.cpp
+++ 2019.11.25.ДЗ/1.cpp
@@ -7,15 +7,15 @@
 using namespace std;
 
 int main() {
-	int y=0,r,i=0,x=0;
+int y=0,r,i,x=0;
 
-	r= 78487894 / 2020;
+r= 78487894 / 2020;
 
-	for (i = 0; i < k; i++) {
-		x = 78487894-y*2021;
-		if (x % 2020 == 0) {
-			cout << x / 2020 << "  " << y << endl;
+for (i = 0; i < r; i++) {
+x = 78487894-y*2021;
+if (x % 2020 == 0) {
+    cout << x / 2020 << "  " << y << endl;
     }
-		y++;
-}
+      y++;
+  }
 }
